4b1362ba2bc3474a9590bed7f91f006bbc1a6a36
Dipak Parmar <hi@dipak.tech>
2023-01-04 08:24:47 -0800

    refactor: improve initconfig function

--- cmd/root.go
+++ cmd/root.go
@@ -61,21 +61,45 @@ func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
+
 	} else {
 		// Find home directory.
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".mta-sts-server" (without extension).
-		viper.AddConfigPath(home)
+		defaultCfgFilePath = home + "/.config/mta-sts-server/config.yaml"
+
+		// Search config in home directory .config/mta-sts-server/config.yaml
+		viper.AddConfigPath(home + "/.config/mta-sts-server")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".mta-sts-server")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
+		if viper.GetBool("verbose") {
+			fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		}
+
+		// validate config
+		validateConfig()
+
+		// override config with env var
+		overrideConfig()
+
+		// if verbose print config
+		if viper.GetBool("verbose") {
+			fmt.Println("Initial config:", viper.AllSettings())
+		}
+	} else {
+		// sdtout create config file
+		fmt.Fprintln(os.Stderr, "Creating config file at", defaultCfgFilePath)
+		createConfig()
+	}
+}
+
 func createConfig() {
 	home, err := os.UserHomeDir()
 	cobra.CheckErr(err)
